922109777ba22bcbb0cd7c241323a0737cb7893a
Tomtestt <142940242+Tomtestt@users.noreply.github.com>
2025-06-08 18:25:13 -0300

    Update AppDbContext.cs

--- TP/61290 - Azubel, Tomas/tp6/servidor/Data/AppDbContext.cs
+++ TP/61290 - Azubel, Tomas/tp6/servidor/Data/AppDbContext.cs
@@ -1,1 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using servidor.Models;
 
+namespace servidor.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Producto> Productos => Set<Producto>();
+    public DbSet<Compra> Compras => Set<Compra>();
+    public DbSet<ItemCompra> ItemsCompra => Set<ItemCompra>();
+}
