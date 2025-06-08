2c043e4204a0a0f6478a186051d8fd759fb68b24
Tomtestt <142940242+Tomtestt@users.noreply.github.com>
2025-06-08 18:50:24 -0300

    Create Cliente.cs

---
+++ TP/61290 - Azubel, Tomas/tp6/cliente/Models/Cliente.cs
@@ -0,0 +1,8 @@
+namespace Cliente.Models;
+
+public class Cliente
+{
+    public string Nombre { get; set; } = "";
+    public string Apellido { get; set; } = "";
+    public string Email { get; set; } = "";
+}
