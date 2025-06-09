88702f84127390729040d10b298da86b36b9b0f6
Francisco Racedo <franciscor829@gmail.com>
2025-06-09 19:27:08 -0300

    Se crea la clase Producto con sus atributos.

--- TP/61064 - Racedo, Francisco Nicolas/tp6/servidor/Program.cs
+++ TP/61064 - Racedo, Francisco Nicolas/tp6/servidor/Program.cs
@@ -63,3 +63,15 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
+
+
+public class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = null!;
+    public string Descripcion { get; set; } = null!;
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+    public string ImagenUrl { get; set; } = null!;
+}
+
