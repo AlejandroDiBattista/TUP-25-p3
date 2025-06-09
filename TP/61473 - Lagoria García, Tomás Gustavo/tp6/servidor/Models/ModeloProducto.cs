85bf72c3693e973991e3207c957b5f0dc56dc1b2
tomaslago27 <116082698+tomaslago27@users.noreply.github.com>
2025-06-09 19:17:15 -0300

    Modelo de la Tabla Producto

---
+++ TP/61473 - Lagoria García, Tomás Gustavo/tp6/servidor/Models/ModeloProducto.cs
@@ -0,0 +1,13 @@
+namespace Proyecto_Tienda_Online.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string ImagenURL { get; set; }
+        
+    }
+}
