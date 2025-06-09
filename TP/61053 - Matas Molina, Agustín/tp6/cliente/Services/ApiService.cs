f4e18fa61960e96530ed51631b5c17a1bdd50fc4
agusmatas004 <ayusmatas@gmail.com>
2025-06-09 13:08:52 -0300

    Agrege metodos para los endpoints

--- TP/61053 - Matas Molina, Agustín/tp6/cliente/Services/ApiService.cs
+++ TP/61053 - Matas Molina, Agustín/tp6/cliente/Services/ApiService.cs
@@ -9,6 +9,7 @@ public class ApiService {
         _httpClient = httpClient;
     }
 
+    
     public async Task<DatosRespuesta> ObtenerDatosAsync() {
         try {
             var response = await _httpClient.GetFromJsonAsync<DatosRespuesta>("/api/datos");
@@ -18,9 +19,44 @@ public class ApiService {
             return new DatosRespuesta { Mensaje = $"Error: {ex.Message}", Fecha = DateTime.Now };
         }
     }
+
+ 
+    public async Task<List<Producto>> GetProductosAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Producto>>("/api/productos");
+    }
+
+
+    public async Task<int> CrearCarritoAsync()
+    {
+        var response = await _httpClient.PostAsync("/api/carritos", null);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadFromJsonAsync<CarritoIdResponse>();
+            return result.carritoId;
+        }
+        throw new Exception("No se pudo crear el carrito");
+    }
+
+
 }
 
 public class DatosRespuesta {
     public string Mensaje { get; set; }
     public DateTime Fecha { get; set; }
+}
+
+public class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+    public string ImagenUrl { get; set; }
+}
+
+public class CarritoIdResponse
+{
+    public int carritoId { get; set; }
 }
