7f947cbd2c2504b58b4b3374a0a86798dbf6f03d
maxilunaa <maxandlu47@gmail.com>
2025-06-09 14:27:00 -0300

    conecto las clases con la base de datos

---
+++ TP/61226 - Luna, Andres Maximiliano/tp6/servidor/AppDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using servidor.Models;
+
+namespace servidor
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<ItemCompra> ItemsCompra { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ItemCompra>()
+                .HasOne(ic => ic.Compra)
+                .WithMany(c => c.Items)
+                .HasForeignKey(ic => ic.CompraId);
+
+            modelBuilder.Entity<ItemCompra>()
+                .HasOne(ic => ic.Producto)
+                .WithMany(p => p.ItemsCompra)
+                .HasForeignKey(ic => ic.ProductoId);
+        }
+    }
+}
