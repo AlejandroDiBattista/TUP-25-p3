52b75e7575d06511bdf45a06a37874404779cc63
Francisco Racedo <franciscor829@gmail.com>
2025-06-09 19:34:46 -0300

    Se crea la clase ItemCompra para registrar qué productos, en qué cantidad y a qué precio fueron comprados en cada compra.

--- TP/61064 - Racedo, Francisco Nicolas/tp6/servidor/Program.cs
+++ TP/61064 - Racedo, Francisco Nicolas/tp6/servidor/Program.cs
@@ -86,3 +86,14 @@ public class Compra
     public List<ItemCompra> Items { get; set; } = new();
 }
 
+public class ItemCompra
+{
+    public int Id { get; set; }
+    public int ProductoId { get; set; }
+    public Producto? Producto { get; set; }
+    public int CompraId { get; set; }
+    public Compra? Compra { get; set; }
+    public int Cantidad { get; set; }
+    public decimal PrecioUnitario { get; set; }
+}
+
