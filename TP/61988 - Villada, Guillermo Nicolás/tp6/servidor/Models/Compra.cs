c96d35efe5b1bea855d18f3038215985b1edf1a1
nicoovillada <nicocrcoc@gmail.com>
2025-06-08 22:30:39 -0300

    tuve que actualizar la clase compra

--- TP/61988 - Villada, Guillermo Nicolás/tp6/servidor/Models/Compra.cs
+++ TP/61988 - Villada, Guillermo Nicolás/tp6/servidor/Models/Compra.cs
@@ -2,11 +2,9 @@ namespace servidor.Models;
 
 public class Compra
 {
-    public int Id { get; set; }
-    public DateTime Fecha { get; set; } = DateTime.Now;
-    public string NombreCliente { get; set; } = string.Empty;
-    public string ApellidoCliente { get; set; } = string.Empty;
-    public string EmailCliente { get; set; } = string.Empty;
+    public Guid Id { get; set; }
+    public Cliente Cliente { get; set; } = new();
     public List<ItemCarrito> Items { get; set; } = new();
-    public decimal Total => Items.Sum(i => i.Importe);
+    public DateTime Fecha { get; set; }
 }
+
