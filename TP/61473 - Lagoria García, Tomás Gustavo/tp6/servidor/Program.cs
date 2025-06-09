c1f0b70727fbec459e8e4153c9fee5d8cf6648f4
tomaslago27 <116082698+tomaslago27@users.noreply.github.com>
2025-06-09 20:02:43 -0300

    Agrego Modelo de datos de Producto a Program.cs
    mismo caso de los otros modelos

--- TP/61473 - Lagoria García, Tomás Gustavo/tp6/servidor/Program.cs
+++ TP/61473 - Lagoria García, Tomás Gustavo/tp6/servidor/Program.cs
@@ -56,3 +56,14 @@ public class ItemCompra
 
 
 }
+
+ public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string ImagenURL { get; set; }
+        
+    }
