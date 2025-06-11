39996b1869e09588ce40f37b6e2695f2bd55158c
patricio-jp <patojimenezpaz@gmail.com>
2025-06-11 11:03:02 -0300

    [Servidor] Agregado EFCore + SQLite y modelo de datos

--- TP/61450 - Jiménez Paz, Patricio Agustín/tp6/servidor/Program.cs
+++ TP/61450 - Jiménez Paz, Patricio Agustín/tp6/servidor/Program.cs
@@ -1,12 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Base de datos usando Entity Framework Core y SQLite
+builder.Services.AddDbContext<AppDb>(opt => opt.UseSqlite("Data Source=./tienda.db"));
+
 // Agregar servicios CORS para permitir solicitudes desde el cliente
-builder.Services.AddCors(options => {
-    options.AddPolicy("AllowClientApp", policy => {
-        policy.WithOrigins("http://localhost:5177", "https://localhost:7221")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowClientApp",
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:5177", "https://localhost:7221")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+    );
 });
 
 // Agregar controladores si es necesario
@@ -15,7 +27,8 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configurar el pipeline de solicitudes HTTP
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
+{
     app.UseDeveloperExceptionPage();
 }
 
@@ -29,3 +42,46 @@ app.MapGet("/", () => "Servidor API está en funcionamiento");
 app.MapGet("/api/datos", () => new { Mensaje = "Datos desde el servidor", Fecha = DateTime.Now });
 
 app.Run();
+
+// Modelo de datos
+class AppDb : DbContext
+{
+    public AppDb(DbContextOptions<AppDb> options)
+        : base(options) { }
+
+    public DbSet<Producto> Productos => Set<Producto>();
+    public DbSet<Compra> Compras => Set<Compra>();
+    public DbSet<ItemCompra> ItemsCompra => Set<ItemCompra>();
+}
+
+// Modelo de datos
+class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public int Stock { get; set; } = 0;
+    public string ImagenUrl { get; set; } = null!;
+}
+
+class Compra
+{
+    public int Id { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    public decimal Total { get; set; }
+    public string NombreCliente { get; set; }
+    public string ApellidoCliente { get; set; }
+    public string EmailCliente { get; set; }
+}
+
+class ItemCompra
+{
+    public int Id { get; set; }
+    public int Cantidad { get; set; }
+    public decimal PrecioUnitario { get; set; }
+    public int ProductoId { get; set; }
+    public Producto Producto { get; set; } = null!;
+    public int CompraId { get; set; }
+    public Compra Compra { get; set; } = null!;
+}
