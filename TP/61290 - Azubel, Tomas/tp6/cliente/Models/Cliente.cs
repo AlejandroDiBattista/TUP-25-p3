cd6b0e8a1fd5225ea7cea162e6b7e2bb6b2759a6
Tomtestt <142940242+Tomtestt@users.noreply.github.com>
2025-06-11 08:55:16 -0300

    Update and rename Cliente.cs to CustomerInfo.cs

--- TP/61290 - Azubel, Tomas/tp6/cliente/Models/Cliente.cs
+++
@@ -1,8 +0,0 @@
-namespace Cliente.Models;
-
-public class Cliente
-{
-    public string Nombre { get; set; } = "";
-    public string Apellido { get; set; } = "";
-    public string Email { get; set; } = "";
-}
