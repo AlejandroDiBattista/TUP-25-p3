e7b972ac15c50fcb28e40f28ab3fa1d3204249b2
maira-acostaa <mairaacostaa2005@gmail.com>
2025-06-10 21:33:41 -0300

    Create TiendaContext.cs

---
+++ TP/61667 - Acosta, Maira/tp6/servidor/Data/TiendaContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using servidor.Models;
+namespace servidor.Data
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options) : base(options)
+        {
+        }
+
+        public Dbset<Producto> Productos => Set<Producto>();
+        public DbSet<Compra> Compras => Set<Compra>();
+        public DbSet<Articulocompra> ArticulosCompras => Set<Articulocompra>();
+    }
+}
