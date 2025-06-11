2759d0d23204e109dfe34119f76269947d607c62
maira-acostaa <mairaacostaa2005@gmail.com>
2025-06-10 21:26:05 -0300

    Create Compra.cs

---
+++ TP/61667 - Acosta, Maira/tp6/servidor/Models/Compra.cs
@@ -0,0 +1,13 @@
+namespace servidor.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Total { get; set; }
+        public string NombreCliente { get; set; } = string.Empty;
+        public string ApellidoCliente { get; set; } = string.Empty;
+        public string EmailCliente { get; set; } = string.Empty;
+        public List<Articulocompra> Articulos { get; set; } = new ();
+    }
+}
