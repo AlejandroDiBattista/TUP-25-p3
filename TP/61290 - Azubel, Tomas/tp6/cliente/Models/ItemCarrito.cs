6ca71fd60e3f121c0548e48cf6dc6e16748aca00
Tomtestt <142940242+Tomtestt@users.noreply.github.com>
2025-06-11 08:55:40 -0300

    Update ItemCarrito.cs

--- TP/61290 - Azubel, Tomas/tp6/cliente/Models/ItemCarrito.cs
+++ TP/61290 - Azubel, Tomas/tp6/cliente/Models/ItemCarrito.cs
@@ -2,10 +2,11 @@ namespace Cliente.Models;
 
 public class ItemCarrito
 {
-    public int Id { get; set; }
-    public string Nombre { get; set; } = "";
-    public decimal Precio { get; set; }
-    public int Stock { get; set; }
-    public string ImagenUrl { get; set; } = "";
+    public int ProductoId { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public decimal PrecioUnitario { get; set; }
     public int Cantidad { get; set; }
+    public string ImagenUrl { get; set; } = string.Empty;
+
+    public decimal Importe => PrecioUnitario * Cantidad;
 }
