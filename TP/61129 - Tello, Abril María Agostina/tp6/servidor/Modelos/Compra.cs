028c542f1beb64e5d74109f720810ec143b908b1
abriltello <abriltello2005@gmail.com>
2025-06-10 22:42:36 -0300

    Commit 1
    Agregue la carpeta modelos a servidor con sus entidades

---
+++ TP/61129 - Tello, Abril María Agostina/tp6/servidor/Modelos/Compra.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Servidor.Modelos  
+{
+    public class Compra 
+    {
+        public int Id { get; set; } 
+        public DateTime Fecha { get; set; } 
+        public decimal Total { get; set; } 
+        public int ClienteId { get; set; }
+
+        public List<ItemCompra> Items { get; set; } = new(); 
+    }
+ 
+}
