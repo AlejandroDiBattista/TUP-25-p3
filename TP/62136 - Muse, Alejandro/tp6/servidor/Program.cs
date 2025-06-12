eb80cc1c9982a5288070f8ead720f97cf9d21606
Muse Alejandro <dantecdd666@gmail.com>
2025-06-11 23:29:26 -0300

    modficacion
    problema con el formato de las imágenes descargadas y la ruta errores, ya modificada

--- TP/62136 - Muse, Alejandro/tp6/servidor/Program.cs
+++ TP/62136 - Muse, Alejandro/tp6/servidor/Program.cs
@@ -58,15 +58,15 @@ using (var scope = app.Services.CreateScope())
     {
         db.Productos.AddRange(
             new Producto { Nombre = "ASUS Zenfone 12 Ultra", Descripcion = "Celular gama alta, 512GB, 12GB RAM", Precio = 950000, Stock = 10, ImagenUrl = "/celulares/ASUS Zenfone 12 Ultra.png" },
-            new Producto { Nombre = "Samsung Galaxy S24 Ultra", Descripcion = "Celular premium, 1TB, 16GB RAM", Precio = 1200000, Stock = 8, ImagenUrl = "/celulares/Samsung Galaxy S24 Ultra.png" },
-            new Producto { Nombre = "iPhone 15 Pro Max", Descripcion = "Apple, 512GB, 12GB RAM", Precio = 1300000, Stock = 7, ImagenUrl = "/celulares/iPhone 15 Pro Max.png" },
+            new Producto { Nombre = "Google Pixel 9 Pro XL", Descripcion = "Google, 256GB, 12GB RAM", Precio = 900000, Stock = 9, ImagenUrl = "/celulares/Google Pixel 9 Pro XL.png" },
+            new Producto { Nombre = "iPhone 16 Pro Max", Descripcion = "Apple, 512GB, 12GB RAM", Precio = 1300000, Stock = 7, ImagenUrl = "/celulares/iPhone 16 Pro Max.png" },
             new Producto { Nombre = "Motorola Edge 50 Pro", Descripcion = "Motorola, 256GB, 8GB RAM", Precio = 650000, Stock = 12, ImagenUrl = "/celulares/Motorola Edge 50 Pro.png" },
-            new Producto { Nombre = "Xiaomi 14 Ultra", Descripcion = "Xiaomi, 512GB, 12GB RAM", Precio = 800000, Stock = 15, ImagenUrl = "/celulares/Xiaomi 14 Ultra.png" },
-            new Producto { Nombre = "Google Pixel 8 Pro", Descripcion = "Google, 256GB, 12GB RAM", Precio = 900000, Stock = 9, ImagenUrl = "/celulares/Google Pixel 8 Pro.png" },
-            new Producto { Nombre = "OnePlus 12", Descripcion = "OnePlus, 512GB, 16GB RAM", Precio = 850000, Stock = 11, ImagenUrl = "/celulares/OnePlus 12.png" },
-            new Producto { Nombre = "Honor Magic 6 Pro", Descripcion = "Honor, 512GB, 12GB RAM", Precio = 780000, Stock = 10, ImagenUrl = "/celulares/Honor Magic 6 Pro.png" },
-            new Producto { Nombre = "Sony Xperia 1 VI", Descripcion = "Sony, 256GB, 12GB RAM", Precio = 950000, Stock = 6, ImagenUrl = "/celulares/Sony Xperia 1 VI.png" },
-            new Producto { Nombre = "Realme GT 6", Descripcion = "Realme, 256GB, 8GB RAM", Precio = 600000, Stock = 14, ImagenUrl = "/celulares/Realme GT 6.png" }
+            new Producto { Nombre = "OnePlus 13", Descripcion = "OnePlus, 512GB, 16GB RAM", Precio = 850000, Stock = 11, ImagenUrl = "/celulares/OnePlus 13.png" },
+            new Producto { Nombre = "OPPO Find X8 Pro", Descripcion = "OPPO, 512GB, 12GB RAM", Precio = 800000, Stock = 8, ImagenUrl = "/celulares/OPPO Find X8 Pro.png" },
+            new Producto { Nombre = "Realme GT 7 Pro", Descripcion = "Realme, 256GB, 8GB RAM", Precio = 600000, Stock = 14, ImagenUrl = "/celulares/Realme GT 7 Pro.png" },
+            new Producto { Nombre = "Samsung Galaxy S25 Ultra", Descripcion = "Celular premium, 1TB, 16GB RAM", Precio = 1200000, Stock = 8, ImagenUrl = "/celulares/Samsung Galaxy S25 Ultra.png" },
+            new Producto { Nombre = "vivo X200 Pro", Descripcion = "vivo, 512GB, 12GB RAM", Precio = 780000, Stock = 10, ImagenUrl = "/celulares/vivo X200 Pro.png" },
+            new Producto { Nombre = "Xiaomi 15 Ultra", Descripcion = "Xiaomi, 512GB, 12GB RAM", Precio = 800000, Stock = 15, ImagenUrl = "/celulares/Xiaomi 15 Ultra.png" }
         );
         db.SaveChanges();
     }
