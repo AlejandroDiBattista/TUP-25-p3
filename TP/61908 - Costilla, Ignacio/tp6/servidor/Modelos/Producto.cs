e9ba3b2fdfca2d1f706d2655c27cf0f8cbd9d5f5
Ignacio-32 <ignacostilla23@gmail.com>
2025-06-13 20:55:34 -0300

    0.1
    modelos (Producto, Compra, ItemCompra) y  TiendaContext

---
+++ TP/61908 - Costilla, Ignacio/tp6/servidor/Modelos/Producto.cs
@@ -0,0 +1,10 @@
+// representa un producto en la tienda
+public class Producto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+    public string ImagenUrl { get; set; }
+}
---
+++ TP/61908 - Costilla, Ignacio/tp6/servidor/TiendaContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+// representa la sesión con la base de datos
+public class TiendaContext : DbContext
+{
+    public TiendaContext(DbContextOptions<TiendaContext> options) : base(options) { }
+
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Compra> Compras { get; set; }
+    public DbSet<ItemCompra> ItemsCompra { get; set; }
+}
