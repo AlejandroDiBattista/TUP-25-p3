a70c678f1f00e0ac04a841e5adfc58f38bb5e0fd
Juanrobledo100 <robledojuan100@gmail.com>
2025-06-13 09:24:15 -0300

    models
    Se realizo la creacion de los modelos

---
+++ TP/62104 - Robledo, Juan Gerardo/tp6/servidor/models/ConfirmarCompraRequest.cs
@@ -0,0 +1,13 @@
+namespace servidor.models
+{
+    public class ConfirmarCompraRequest
+    {
+        [Required]
+        public string NombreCliente { get; set; } = string.Empty;
+        [Required]
+        public string ApellidoCliente { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string EmailCliente { get; set; } = string.Empty;
+    }
+}
