52cf0f24879d1ec0fb664ef7a3b4db20553141fa
agus-op15 <agustin.emadiaz@gmail.com>
2025-06-13 17:27:30 -0300

    Cambio de nombre de App a Application para verificar que el backend funciona
    El backend funciona correctamente

--- TP/61051 - Diaz, Agustin Emanuel/tp6/servidor/Program.cs
+++ TP/61051 - Diaz, Agustin Emanuel/tp6/servidor/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=tienda.db"));
 
 builder.Services.AddCors(options => {
@@ -229,7 +229,7 @@ app.MapDelete("/carritos/{id}", ([FromRoute] Guid id) =>
 
 using (var scope = app.Services.CreateScope())
 {
-  var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+  var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
   db.Database.EnsureCreated();
 
   if (!db.Productos.Any())
