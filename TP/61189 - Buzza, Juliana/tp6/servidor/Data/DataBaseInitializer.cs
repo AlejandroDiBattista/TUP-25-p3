bc7a99d2c815b3ddf778f85cd117c6c1c9b9d695
julianabuzza <buzzajuliana05@gmail.com>
2025-06-12 23:29:17 -0300

    Página Home (Catálogo de Productos) con funcionalidad de agregar al carrito
    Página principal (Home.razor) de la tienda online. Muestra un catálogo de productos en formato de tarjetas, cada una con la imagen, nombre, descripción, stock y precio del producto

--- TP/61189 - Buzza, Juliana/tp6/servidor/Data/DataBaseInitializer.cs
+++ TP/61189 - Buzza, Juliana/tp6/servidor/Data/DataBaseInitializer.cs
@@ -16,16 +16,16 @@ namespace servidor.Data
 
             var productos = new Producto[]
             {
-                new Producto{Nombre="Zapatilla Urbana Clásica", Descripcion="Comodidad y estilo para el día a día.", Precio=75.00m, Stock=50, ImagenUrl="/images/Productos/zapatilla_urbana_clasica.jpg"},
-                new Producto{Nombre="Botín de Cuero Elegante", Descripcion="Ideal para ocasiones formales, durabilidad garantizada.", Precio=120.00m, Stock=30, ImagenUrl="/images/Productos/botin_cuero_elegante.jpg"},
-                new Producto{Nombre="Sandalia de Verano", Descripcion="Frescura y ligereza para tus días soleados.", Precio=45.00m, Stock=70, ImagenUrl="/images/Productos/sandalia_verano.jpg"},
-                new Producto{Nombre="Deportivo de Running", Descripcion="Máximo rendimiento y amortiguación para corredores.", Precio=90.00m, Stock=40, ImagenUrl="/images/Productos/deportivo_running.jpg"},
-                new Producto{Nombre="Zapato Oxford Casual", Descripcion="Versatilidad para combinar con cualquier atuendo.", Precio=85.00m, Stock=25, ImagenUrl="/images/Productos/zapato_oxford_casual.jpg"},
-                new Producto{Nombre="Bota de Montaña Impermeable", Descripcion="Protección y agarre para tus aventuras al aire libre.", Precio=150.00m, Stock=20, ImagenUrl="/images/Productos/bota_montana_impermeable.jpg"},
-                new Producto{Nombre="Mocasín Confort", Descripcion="Suavidad y adaptabilidad para un uso prolongado.", Precio=60.00m, Stock=60, ImagenUrl="/images/Productos/mocasin_confort.jpg"},
-                new Producto{Nombre="Stiletto Negro Clásico", Descripcion="Elegancia atemporal para complementar tu look de noche.", Precio=100.00m, Stock=35, ImagenUrl="/images/Productos/stiletto_negro_clasico.jpg"},
-                new Producto{Nombre="Zapatilla de Skate", Descripcion="Agarre y resistencia para trucos y uso diario.", Precio=70.00m, Stock=45, ImagenUrl="/images/Productos/zapatilla_skate.jpg"},
-                new Producto{Nombre="Alpargata de Yute", Descripcion="Estilo rústico y natural para un toque veraniego.", Precio=35.00m, Stock=80, ImagenUrl="/images/Productos/alpargata_yute.jpg"}
+                new Producto{Nombre="Chata Mary jane Pine", Descripcion="Chata mary jane 100% cuero color negro liso, brillo medio.", Precio=75.00m, Stock=50, ImagenUrl="/images/Productos/chatamaryjane.jpg"},
+                new Producto{Nombre="Bota Cata Caña Alta", Descripcion="Bota caña alta de cuero color negro liso y leve brillo.", Precio=120.00m, Stock=30, ImagenUrl="/images/Productos/botacatanegro.jpg"},
+                new Producto{Nombre="Bota Cata Caña Alta Marron", Descripcion="Bota caña alta de color marron oxidado y leve brillo.", Precio=45.00m, Stock=70, ImagenUrl="/images/Productos/botacatamarron.jpg"},
+                new Producto{Nombre="Sueco Andi Negro", Descripcion="Sueco 100% de cuero liso color negro brillante.", Precio=90.00m, Stock=40, ImagenUrl="/images/Productos/suecoandinegro.jpg"},
+                new Producto{Nombre="Sueco Andi Merlot", Descripcion="Sueco 100% cuero de cuero liso color bordo intenso.", Precio=85.00m, Stock=25, ImagenUrl="/images/Productos/suecoandi.jpg"},
+                new Producto{Nombre="Mocasin Zoe Black", Descripcion="Mocasin 100% cuero liso, brillo medio, color negro.", Precio=150.00m, Stock=20, ImagenUrl="/images/Productos/mocasinzoenegro.jpg"},
+                new Producto{Nombre="Sandalia Malibu Black", Descripcion="Sandalias 100% de cuero negro liso.", Precio=60.00m, Stock=60, ImagenUrl="/images/Productos/sandaliasnegro.jpg"},
+                new Producto{Nombre="Ojota Pine Black", Descripcion="Sandalia ojota de cuero liso.", Precio=100.00m, Stock=35, ImagenUrl="/images/Productos/ojotanegro.jpg"},
+                new Producto{Nombre="Franciscana Taylor Óxido", Descripcion="Sandalia modelo franciscana de cuero efecto óxido", Precio=70.00m, Stock=45, ImagenUrl="/images/Productos/sandaliasoxido.jpg"},
+                new Producto{Nombre="Bota China Cacao", Descripcion="Bota caña alta de cuero gamuzado color marron.", Precio=35.00m, Stock=80, ImagenUrl="/images/Productos/botachina.jpg"}
             };
 
             foreach (Producto p in productos)
