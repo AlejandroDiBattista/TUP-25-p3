5a1614b54e490c2dbeaa74042b1313d840fe77ec
tomaslago27 <116082698+tomaslago27@users.noreply.github.com>
2025-06-12 22:52:50 -0300

    modelo del item
    modelo de cuales serán los campos del item del carrito

--- TP/61473 - Lagoria García, Tomás Gustavo/tp6/cliente/Services/CarritoService.cs
+++ TP/61473 - Lagoria García, Tomás Gustavo/tp6/cliente/Services/CarritoService.cs
@@ -60,3 +60,10 @@ public async Task GuardarCarritoIdAsync(Guid carritoId)
         await _httpClient.DeleteAsync($"carritos/{carritoId}/{productoId}");
     }
 
+public class ItemCarrito
+{
+    public int ProductoId { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public decimal PrecioUnitario { get; set; }
+    public int Cantidad { get; set; }
+}
