f2eb79c9efe4ff3db096ff2c347c15cced188046
Mario Moussa <mariomoussa6@gmail.com>
2025-06-14 14:04:18 -0300

    commit 13

--- TP/61328 - Moussa Cancino, Mario Matias/tp6/cliente/Modelos/DatosCliente.cs
+++ TP/61328 - Moussa Cancino, Mario Matias/tp6/cliente/Modelos/DatosCliente.cs
@@ -5,13 +5,13 @@ namespace cliente.Modelos
     public class DatosCliente
     {
         [Required(ErrorMessage = "El nombre es obligatorio.")]
-        public string Nombre { get; set; }
+        public string NombreCliente { get; set; } 
 
         [Required(ErrorMessage = "El apellido es obligatorio.")]
-        public string Apellido { get; set; }
+        public string ApellidoCliente { get; set; } 
 
         [Required(ErrorMessage = "El email es obligatorio.")]
         [EmailAddress(ErrorMessage = "El formato del email no es válido.")]
-        public string Email { get; set; }
+        public string EmailCliente { get; set; } 
     }
 }
--- TP/61328 - Moussa Cancino, Mario Matias/tp6/cliente/Program.cs
+++ TP/61328 - Moussa Cancino, Mario Matias/tp6/cliente/Program.cs
@@ -7,14 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// --- INICIA CORRECCIÓN ---
-// HttpClient y ApiService ahora se registran como Singleton 
-// para que el CartService (que también es Singleton) pueda usarlos.
+
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5184") });
 builder.Services.AddSingleton<ApiService>();
-// --- FIN CORRECCIÓN ---
-
-// CartService se mantiene como Singleton, lo cual es correcto.
 builder.Services.AddSingleton<CartService>();
 
 await builder.Build().RunAsync();
