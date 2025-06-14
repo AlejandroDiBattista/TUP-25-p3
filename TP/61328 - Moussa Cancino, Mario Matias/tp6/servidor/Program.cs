652763a55a7b50acfb881f7de810f9ba3e596b9b
Mario Moussa <mariomoussa6@gmail.com>
2025-06-14 03:02:24 -0300

    5to commit
    incremento de la cantidad del carrito

--- TP/61328 - Moussa Cancino, Mario Matias/tp6/servidor/Program.cs
+++ TP/61328 - Moussa Cancino, Mario Matias/tp6/servidor/Program.cs
@@ -26,11 +26,11 @@ if (app.Environment.IsDevelopment()) {
     app.UseDeveloperExceptionPage();
 }
 
-
 app.UseCors("AllowClientApp");
 
+
+
 app.MapGet("/api/productos", async (TiendaDbContext db, string? q) => {
-    
     var productosQuery = db.Productos.AsQueryable();
     if (!string.IsNullOrWhiteSpace(q))
     {
@@ -48,6 +48,61 @@ app.MapPost("/api/carritos", async (TiendaDbContext db) => {
     db.Carritos.Add(nuevoCarrito);
     await db.SaveChangesAsync();
     return Results.Created($"/api/carritos/{nuevoCarrito.Id}", new { carritoId = nuevoCarrito.Id });
+});
+
+
+app.MapPut("/api/carritos/{carritoId:guid}/productos/{productoId:int}", async (Guid carritoId, int productoId, TiendaDbContext db) => {
+    
+    var carrito = await db.Carritos.Include(c => c.Items).FirstOrDefaultAsync(c => c.Id == carritoId);
+    if (carrito is null)
+    {
+        return Results.NotFound("El carrito no existe.");
+    }
+
+    
+    var producto = await db.Productos.FindAsync(productoId);
+    if (producto is null)
+    {
+        return Results.NotFound("El producto no existe.");
+    }
+
+    
+    var itemEnCarrito = carrito.Items.FirstOrDefault(i => i.ProductoId == productoId);
+
+    if (itemEnCarrito is not null)
+    {
+        
+        if (producto.Stock > itemEnCarrito.Cantidad)
+        {
+            itemEnCarrito.Cantidad++;
+        }
+        else
+        {
+            return Results.BadRequest("No hay suficiente stock para agregar otra unidad de este producto.");
+        }
+    }
+    else
+    {
+      
+        if (producto.Stock > 0)
+        {
+            var nuevoItem = new CarritoItem
+            {
+                ProductoId = producto.Id,
+                Cantidad = 1
+            };
+            carrito.Items.Add(nuevoItem);
+        }
+        else
+        {
+            return Results.BadRequest("Producto sin stock.");
+        }
+    }
+
+    await db.SaveChangesAsync();
+    
+
+    return Results.Ok(carrito);
 });
 
 
