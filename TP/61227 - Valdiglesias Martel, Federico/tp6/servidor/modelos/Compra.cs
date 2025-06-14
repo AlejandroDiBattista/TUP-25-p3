2f1b5aab36384d947736e6304408119e89e965b0
Federico Valdiglesias <federicomrtl@gmail.com>
2025-06-14 20:49:38 -0300

    Create Compra.cs
    Se creo la clase ItemCompra

---
+++ TP/61227 - Valdiglesias Martel, Federico/tp6/servidor/modelos/Compra.cs
@@ -0,0 +1,9 @@
+public class Compra
+{
+    public int Id { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.UtcNow;
+    public decimal Total { get; set; }
+    public string NombreCliente { get; set; } = string.Empty;
+    public string ApellidoCliente { get; set; } = string.Empty;
+    public string EmailCliente { get; set; } = string.Empty;
+}
