a2011b58785f7b67e8890dc30a8fb27867e3f7aa
Juan-PH <juanpablo-heredia@hotmail.com>
2025-06-13 22:34:51 -0300

    0.0.3
    Cargando datos en la BD, agregando imagenes en carpeta img en el servidor

--- TP/61652 - Heredia, Juan Pablo/tp6/servidor/Program.cs
+++ TP/61652 - Heredia, Juan Pablo/tp6/servidor/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TiendaDb>(options =>
     options.UseSqlite("Data Source=tienda.db"));
-    
+
 // Agregar servicios CORS para permitir solicitudes desde el cliente
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowClientApp", policy => {
@@ -20,9 +20,33 @@ builder.Services.AddCors(options => {
 builder.Services.AddControllers();
 
 var app = builder.Build();
+app.UseStaticFiles();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<TiendaDb>();
+    db.Database.EnsureCreated();
+    if (!db.Productos.Any())
+    {
+        db.Productos.AddRange(new List<Producto> {
+            new Producto { Nombre = "Auriculares", Descripcion = "Ares H120 RGB", Precio = 120000, Stock = 10, ImagenUrl = "http://localhost:5184/img/ARES-H120-RGB.png" },
+            new Producto { Nombre = "Auriculares", Descripcion = "Pandora H350", Precio = 110000, Stock = 20, ImagenUrl = "http://localhost:5184/img/PANDORA-H350.png" },
+            new Producto { Nombre = "Auriculares", Descripcion = "Zeus x Wireless H510", Precio = 250000, Stock = 6, ImagenUrl = "http://localhost:5184/img/ZEUS-X-WIRELESS-H510.png" },
+            new Producto { Nombre = "Mouse", Descripcion = "Storm Elite", Precio = 20500, Stock = 30, ImagenUrl = "http://localhost:5184/img/STORM-ELITE.png" },
+            new Producto { Nombre = "Mouse", Descripcion = "Invader M719", Precio = 16000, Stock = 14, ImagenUrl = "http://localhost:5184/img/INVADER-M719.png" },
+            new Producto { Nombre = "Mouse", Descripcion = "Impact M908", Precio = 15000, Stock = 8, ImagenUrl = "http://localhost:5184/img/IMPACT-M908.png" },
+            new Producto { Nombre = "Teclado", Descripcion = "Kumara K522", Precio = 10000, Stock = 24, ImagenUrl = "http://localhost:5184/img/KUMARA-K552.png" },
+            new Producto { Nombre = "Teclado", Descripcion = "Deimos K599", Precio = 19000, Stock = 12, ImagenUrl = "http://localhost:5184/img/DEIMOS-K599.png" },
+            new Producto { Nombre = "Teclado", Descripcion = "Ziggs K669", Precio = 20000, Stock = 8, ImagenUrl = "http://localhost:5184/img/ZIGGS-K669.png" },
+            new Producto { Nombre = "Teclado", Descripcion = "DRAGONBORN K630", Precio = 25000, Stock = 16, ImagenUrl = "http://localhost:5184/img/DRAGONBORN-K630.png" }
+        });
+        db.SaveChanges();
+    }
+}
 
 // Configurar el pipeline de solicitudes HTTP
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
+{
     app.UseDeveloperExceptionPage();
 }
 
