78d4808a8c8330a13c72df56f7143b41193e1020
Federico Valdiglesias <federicomrtl@gmail.com>
2025-06-14 22:16:14 -0300

    Update Compra.cs
    Correcion de la clase compra.cs

--- TP/61227 - Valdiglesias Martel, Federico/tp6/servidor/modelos/Compra.cs
+++ TP/61227 - Valdiglesias Martel, Federico/tp6/servidor/modelos/Compra.cs
@@ -3,7 +3,7 @@ public class Compra
     public int Id { get; set; }
     public DateTime Fecha { get; set; } = DateTime.UtcNow;
     public decimal Total { get; set; }
-    public string NombreCliente { get; set; } = string.Empty;
-    public string ApellidoCliente { get; set; } = string.Empty;
-    public string EmailCliente { get; set; } = string.Empty;
+    public string NombreCliente { get; set; } = "";
+    public string ApellidoCliente { get; set; } = "";
+    public string EmailCliente { get; set; } = "";
 }
