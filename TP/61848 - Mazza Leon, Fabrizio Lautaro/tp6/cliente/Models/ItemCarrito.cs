c40f1fefaed9d5efd3e456682fef6a33c200f18d
FabriMazza <fabri.mazza15@gmail.com>
2025-06-14 22:18:17 -0300

    Item del carrito

--- TP/61848 - Mazza Leon, Fabrizio Lautaro/tp6/cliente/Models/ItemCarrito.cs
+++ TP/61848 - Mazza Leon, Fabrizio Lautaro/tp6/cliente/Models/ItemCarrito.cs
@@ -2,7 +2,9 @@ namespace cliente.Models
 {
     public class ItemCarrito
     {
+        public int ProductoId { get; set; }
         public Producto Producto { get; set; }
         public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
     }
 }
