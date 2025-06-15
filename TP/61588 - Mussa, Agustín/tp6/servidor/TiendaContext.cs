0dda548b21d1da7ffb8e8ac545e428b13b9e439d
AgustinCJM <agustinmussa9@gmail.com>
2025-06-15 00:09:04 -0300

    commit 4
    implementacion de modelos

--- TP/61588 - Mussa, Agustín/tp6/servidor/TiendaContext.cs
+++ TP/61588 - Mussa, Agustín/tp6/servidor/TiendaContext.cs
@@ -48,16 +48,16 @@ namespace servidor
                 {
                     db.Productos.AddRange(new List<Producto>
                     {
-                        new Producto { Nombre = "Producto 1", Descripcion = "Silla Gamer Vertagear SL5800 HygennX Carbon Black Ergonomic", Precio = 497.596M, Stock = 10, ImagenUrl = "/images/producto1.jpg" },
-                        new Producto { Nombre = "Producto 2", Descripcion = "Mother Asrock B660M-HDV S1700 DDR4", Precio = 900.000M, Stock = 5, ImagenUrl = "/images/producto2.jpg" },
-                        new Producto { Nombre = "Producto 3", Descripcion = "Monitor Lenovo ThinkVision S22i-30 21.5 FHD IPS 75Hz Anti Glare VESA", Precio = 149.999M, Stock = 5, ImagenUrl = "/images/producto3.jpg" },
-                        new Producto { Nombre = "Producto 4", Descripcion = "Procesador Intel Core i9 14900K 6.0GHz Turbo Socket 1700 Raptor Lake", Precio = 673.665M, Stock = 0, ImagenUrl = "/images/producto4.jpg" },
-                        new Producto { Nombre = "Producto 5", Descripcion = "Fuente Be Quiet 1000W 80 Plus Platinum STRAIGHT POWER 12 Full Modular ATX 3.1 PCIe 5.1", Precio = 306.450M, Stock = 20, ImagenUrl = "/images/producto5.jpg" },
-                        new Producto { Nombre = "Producto 6", Descripcion = "Memoria Corsair DDR5 64GB (2x32GB) 6000MHz Dominator Titanium RGB CL30 Intel XMP3.0/AMD EXPO ICUE", Precio = 433.799M, Stock = 15, ImagenUrl = "/images/producto6.jpg" },
-                        new Producto { Nombre = "Producto 7", Descripcion = "Teclado Hipermagnético Steel Series Apex PRO TKL 3 Wireless 2.4Ghz Bluetooth Black RGB Omnipoint 3.0 Hall Effect", Precio = 332.100M, Stock = 8, ImagenUrl = "/images/producto7.jpg" },
-                        new Producto { Nombre = "Producto 8", Descripcion = "Mouse Logitech G Pro X Superlight Wireless", Precio = 120.000M, Stock = 12, ImagenUrl = "/images/producto8.jpg" },
-                        new Producto { Nombre = "Producto 9", Descripcion = "Auriculares HyperX Cloud II", Precio = 80.000M, Stock = 7, ImagenUrl = "/images/producto9.jpg" },
-                        new Producto { Nombre = "Producto 10", Descripcion = "Pad Mouse Redragon Flick XL", Precio = 15.000M, Stock = 25, ImagenUrl = "/images/producto10.jpg" }
+                        new Producto { Nombre = "Producto 1", Descripcion = "Silla Gamer Vertagear SL5800 HygennX Carbon Black Ergonomic", Precio = 497.596M, Stock = 10, ImagenUrl = "/img/Captura1.png" },
+                        new Producto { Nombre = "Producto 2", Descripcion = "Mother Asrock B660M-HDV S1700 DDR4", Precio = 900.000M, Stock = 5, ImagenUrl = "/img/Captura2.png" },
+                        new Producto { Nombre = "Producto 3", Descripcion = "Monitor Lenovo ThinkVision S22i-30 21.5 FHD IPS 75Hz Anti Glare VESA", Precio = 149.999M, Stock = 5, ImagenUrl = "/img/Captura3.png" },
+                        new Producto { Nombre = "Producto 4", Descripcion = "Procesador Intel Core i9 14900K 6.0GHz Turbo Socket 1700 Raptor Lake", Precio = 673.665M, Stock = 0, ImagenUrl = "/img/Captura4.png" },
+                        new Producto { Nombre = "Producto 5", Descripcion = "Fuente Be Quiet 1000W 80 Plus Platinum STRAIGHT POWER 12 Full Modular ATX 3.1 PCIe 5.1", Precio = 306.450M, Stock = 20, ImagenUrl = "/img/Captura5.png" },
+                        new Producto { Nombre = "Producto 6", Descripcion = "Memoria Corsair DDR5 64GB (2x32GB) 6000MHz Dominator Titanium RGB CL30 Intel XMP3.0/AMD EXPO ICUE", Precio = 433.799M, Stock = 15, ImagenUrl = "/img/Captura6.png" },
+                        new Producto { Nombre = "Producto 7", Descripcion = "Teclado Hipermagnético Steel Series Apex PRO TKL 3 Wireless 2.4Ghz Bluetooth Black RGB Omnipoint 3.0 Hall Effect", Precio = 332.100M, Stock = 8, ImagenUrl = "/img/Captura7.png" },
+                        new Producto { Nombre = "Producto 8", Descripcion = "Mouse Logitech G Pro X Superlight Wireless", Precio = 120.000M, Stock = 12, ImagenUrl = "/img/Captura8.png" },
+                        new Producto { Nombre = "Producto 9", Descripcion = "Auriculares HyperX Cloud II", Precio = 80.000M, Stock = 7, ImagenUrl = "/img/Captura9.png" },
+                        new Producto { Nombre = "Producto 10", Descripcion = "Pad Mouse Redragon Flick XL", Precio = 15.000M, Stock = 25, ImagenUrl = "/img/Captura10.png" }
                     });
                     db.SaveChanges();
                 }
