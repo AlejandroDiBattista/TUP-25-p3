753bb506086cb0e1a304f59efff5863670e29f8e
Abi <fmediciassaf@gmail.com>
2025-06-15 00:05:49 -0300

    Upddate 11
    Cambios en la estetica de la pagina + arreglos en base de dato + nueva migracion por el arreglo

--- TP/61247 - Medici Assaf, Abril/tp6/servidor/Migrations/20250615024120_NombreDeLaMigracionUltim.Designer.cs
+++ TP/61247 - Medici Assaf, Abril/tp6/servidor/Migrations/20250615024120_NombreDeLaMigracionUltim.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Servidor.Models;
 
@@ -10,9 +11,11 @@ using Servidor.Models;
 namespace servidor.Migrations
 {
     [DbContext(typeof(PaginaContext))]
-    partial class PaginaContextModelSnapshot : ModelSnapshot
+    [Migration("20250615024120_NombreDeLaMigracionUltim")]
+    partial class NombreDeLaMigracionUltim
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.5");
--- TP/61247 - Medici Assaf, Abril/tp6/servidor/Models/PaginaDbContext.cs
+++ TP/61247 - Medici Assaf, Abril/tp6/servidor/Models/PaginaDbContext.cs
@@ -15,12 +15,12 @@ public class PaginaContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Producto>().HasData(
-        new Producto { ProductoId = 1, Titulo = "Baggie Crema", Detalle = "Pantalon de algodon con estampado.", Valor = 35200, CantidadDisponible = 12, ImagenUrl = "img.catalogo/baggie_blanco.jpeg" },
+        new Producto { ProductoId = 1, Titulo = "Baggie Crema", Detalle = "Pantalon de algodon con estampado.", Valor = 35200, CantidadDisponible = 12, ImagenUrl = "img.catalogo/baggie_blanco.jpg" },
         new Producto { ProductoId = 2, Titulo = "Baggie Negro", Detalle = "Pantalon de algodon con estampado.", Valor = 35200, CantidadDisponible = 8, ImagenUrl = "img.catalogo/baggie_negro.jpeg" },
         new Producto { ProductoId = 3, Titulo = "Bermuda", Detalle = "Bermuda de jean", Valor = 25000, CantidadDisponible = 15, ImagenUrl = "img.catalogo/bermuda.jpeg" },
         new Producto { ProductoId = 4, Titulo = "Buzo marron", Detalle = "Buzo de algodon bordado.", Valor = 40000, CantidadDisponible = 15, ImagenUrl = "img.catalogo/buzo_marron.jpg" },
         new Producto { ProductoId = 5, Titulo = "Buzo negro", Detalle = "Buzo de algodon bordado", Valor = 40000, CantidadDisponible = 15, ImagenUrl = "img.catalogo/buzo_negro.jpeg" },
-        new Producto { ProductoId = 6, Titulo = "Camisa negra", Detalle = "Camisa mangas cortas", Valor = 37000, CantidadDisponible = 15, ImagenUrl = "img.catalogo/camisa.jpeg" },
+        new Producto { ProductoId = 6, Titulo = "Camisa negra", Detalle = "Camisa mangas cortas", Valor = 37000, CantidadDisponible = 15, ImagenUrl = "img.catalogo/camisa.jpg" },
         new Producto { ProductoId = 8, Titulo = "Gorra", Detalle = "Gorra gris gastado", Valor = 15000, CantidadDisponible = 15, ImagenUrl = "img.catalogo/gorra.jpeg" },
         new Producto { ProductoId = 9, Titulo = "Jean Ahumado", Detalle = "Pantalon de jean con bordado", Valor = 40000, CantidadDisponible = 15, ImagenUrl = "img.catalogo/jean_logo.jpeg" },
         new Producto { ProductoId = 10, Titulo = "Pantalon cargo", Detalle = "Pantalon cargo camuflado gris", Valor = 41500, CantidadDisponible = 15, ImagenUrl = "img.catalogo/pantalon_camuflado.jpeg" }
