5e9fdbb27e7569fcce83564155cd914eadcaa4b8
julieta2815a <132948756+julieta2815a@users.noreply.github.com>
2025-06-14 23:17:21 -0300

    commit 1
    hago creacion de modelos de datos con (compras.cs, itemcompras y productos.cs)

---
+++ TP/61611 - Coronel, Julieta Antonella/tp6/servidor/Modelos/Compras.cs
@@ -0,0 +1,10 @@
+public class Compras
+{
+    public int Id { get; set; }
+    public string Fecha { get; set; }
+    public decimal Total { get; set; }
+    public string NombreCliente { get; set; }
+    public string ApellidoCliente { get; set; }
+    public string EmailCliente { get; set; }
+
+}
