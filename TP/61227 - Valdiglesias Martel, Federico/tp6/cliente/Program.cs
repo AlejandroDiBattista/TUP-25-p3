03be3a5735cec781757d9e612aff2e22f66003b0
Federico Valdiglesias <federicomrtl@gmail.com>
2025-06-14 22:41:58 -0300

    Update Program.cs
    Se reconfiguro el archivo Program.cs

--- TP/61227 - Valdiglesias Martel, Federico/tp6/cliente/Program.cs
+++ TP/61227 - Valdiglesias Martel, Federico/tp6/cliente/Program.cs
@@ -1,16 +1,8 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using cliente;
-using cliente.Services;
-
+using cliente.Servicios;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-// Configurar el HttpClient para apuntar al servidor API
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5184") });
-
-// Registrar el servicio API
-builder.Services.AddScoped<ApiService>();
-
+builder.Services.AddScoped<ServicioApi>();
+builder.Services.AddSingleton<EstadoCarrito>();
 await builder.Build().RunAsync();
