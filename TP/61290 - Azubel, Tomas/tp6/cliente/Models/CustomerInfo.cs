17581e53cdd5cc695f17b76e92aa4c909eefb816
Tomtestt <142940242+Tomtestt@users.noreply.github.com>
2025-06-17 19:38:16 -0300

    Delete TP/61290 - Azubel, Tomas/tp6/cliente directory

--- TP/61290 - Azubel, Tomas/tp6/cliente/Models/CustomerInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace cliente.Models;
-
-public class CustomerInfo
-{
-    public string FirstName { get; set; } = "";
-    public string LastName { get; set; } = "";
-    public string Email { get; set; } = "";
-}
