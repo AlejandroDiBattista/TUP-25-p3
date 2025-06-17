c9a63251fda1dc5211758c4346a7651083a97550
Oscar40997 <Ignaciososa1ro@gmail.com>
2025-06-17 09:58:12 -0300

    reconfigurando

--- TP/61136 - Sosa, Oscar Ignacio/tp6/cliente/Services/CartService.cs
+++ TP/61136 - Sosa, Oscar Ignacio/tp6/cliente/Services/CartService.cs
@@ -1,30 +1,33 @@
 using System.Collections.Generic;
-
-public class CartService
+namespace Cliente.Services
 {
-    public List<Product> Items { get; } = new();
-
-    public event Action? OnChange;
-
-    public void AddToCart(Product product)
+    public class CartService
     {
-        Items.Add(product);
-        OnChange?.Invoke();
-    }
-public void RemoveFromCart(Product product)
-    {
-        Items.Remove(product);
-        OnChange?.Invoke();
+        public List<Product> Items { get; } = new();
+
+        public event Action? OnChange;
+
+        public void AddToCart(Product product)
+        {
+            Items.Add(product);
+            OnChange?.Invoke();
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            Items.Remove(product);
+            OnChange?.Invoke();
+        }
+ public void ClearCart()
+        {
+            Items.Clear();
+            OnChange?.Invoke();
+        }
     }
 
-    public void ClearCart()
+    public class Product
     {
-        Items.Clear();
-        OnChange?.Invoke();
+        public string Name { get; set; }
+        public decimal Price { get; set; }
     }
-}
-public class Product
-{
-    public string Name { get; set; }
-    public decimal Price { get; set; }
 }
