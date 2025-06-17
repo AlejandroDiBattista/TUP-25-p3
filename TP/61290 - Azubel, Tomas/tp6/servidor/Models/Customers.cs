1cd4b591552cba9dee5a1537cd81d23d6f92d6c7
Tomtestt <142940242+Tomtestt@users.noreply.github.com>
2025-06-17 19:38:34 -0300

    Delete TP/61290 - Azubel, Tomas/tp6/servidor directory

--- TP/61290 - Azubel, Tomas/tp6/servidor/Models/Customers.cs
+++
@@ -1,8 +0,0 @@
-namespace servidor.Models;
-
-public class Customer
-{
-    public string FirstName { get; set; } = "";
-    public string LastName { get; set; } = "";
-    public string Email { get; set; } = "";
-}
