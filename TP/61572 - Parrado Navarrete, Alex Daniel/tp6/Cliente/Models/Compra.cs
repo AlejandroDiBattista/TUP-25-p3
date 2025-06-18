9cd2cf227be1c808749a7430fd7a0b39bad45e91
Alex <matatitanes1@gmail.com>
2025-06-18 11:47:50 -0300

    61572
    Empiezo con la creacion de los modelos con su respectivo nombre

--- TP/61572 - Parrado Navarrete, Alex Daniel/tp6/Cliente/Models/Compra.cs
+++ TP/61572 - Parrado Navarrete, Alex Daniel/tp6/Cliente/Models/Compra.cs
@@ -1,11 +1,12 @@
-namespace TiendaOnline.Cliente.Models;
+namespace TiendaOnline.Server.Models;
 
 public class Compra
 {
-    public int    Id              { get; set; }
-    public DateTime Fecha         { get; set; }
-    public decimal Total          { get; set; }
-    public string NombreCliente   { get; set; } = string.Empty;
+    public int Id { get; set; }
+    public DateTime Fecha { get; set; }
+    public decimal Total { get; set; }
+    public string NombreCliente { get; set; } = string.Empty;
     public string ApellidoCliente { get; set; } = string.Empty;
-    public string EmailCliente    { get; set; } = string.Empty;
+    public string EmailCliente { get; set; } = string.Empty;
+    public List<ItemCompra> Items { get; set; } = new();
 }
