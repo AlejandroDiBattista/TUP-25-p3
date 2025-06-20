3d484dad2de013ed5524e9d5f3d09f92db3d5701
7GabrielC <gabriel7carabajal@gmail.com>
2025-06-19 23:02:41 -0300

    Cargo Productos en TiendaContext

--- TP/61319 - Carabajal, José Gabriel/tp6/servidor/Data/TiendaContext.cs
+++ TP/61319 - Carabajal, José Gabriel/tp6/servidor/Data/TiendaContext.cs
@@ -28,28 +28,101 @@ namespace servidor.ModelosServidor
         {
             base.OnModelCreating(modelBuilder);
 
-            // Pre‑cargar datos de ejemplo
+            
             modelBuilder.Entity<Producto>().HasData(
                 
                 new Producto
                 {
                     Id = 1,
-                    Nombre = "iPhone 13 Pro",
-                    Descripcion = "Color Blue",
+                    Nombre = "iPhone 16",
+                    Descripcion = "Color: Ultramarine",
                     Precio = 650000m,
-                    Stock = 15,
-                    ImagenUrl = "url1"
+                    Stock = 20,
+                    ImagenUrl = "iPhone16.jpg"
                 },
                 
                 new Producto
                 {
                     Id = 2,
-                    Nombre = "iPhone 12",
-                    Descripcion = "Color Black",
+                    Nombre = "iPhone 15",
+                    Descripcion = "Color: Pink",
                     Precio = 420000m,
-                    Stock = 5,
-                    ImagenUrl = "url2"
+                    Stock = 20,
+                    ImagenUrl = "iPhone15.jpg"
+                },
+                new Producto
+                {
+                    Id = 3,
+                    Nombre = "iPhone 14 Pro",
+                    Descripcion = "Color: Gold",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "iPhone14Pro.jpg"
+                },
+                new Producto
+                {
+                    Id = 4,
+                    Nombre = "iPhone 13 Pro",
+                    Descripcion = "Color: Graphite",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "iPhone13Pro.jpg"
+                },
+                new Producto
+                {
+                    Id = 5,
+                    Nombre = "iPhone 12",
+                    Descripcion = "Color: Red",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "iPhone12.jpg"
+                },
+                new Producto
+                {
+                    Id = 6,
+                    Nombre = "iPhone 11",
+                    Descripcion = "Color: White",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "iPhone11.jpg"
+                },
+                new Producto
+                {
+                    Id = 7,
+                    Nombre = "iPhone XR",
+                    Descripcion = "Color: Blue",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "iPhone16.jpg"
+                },
+                new Producto
+                {
+                    Id = 8,
+                    Nombre = "Funda Silicona iPhone 12",
+                    Descripcion = "Color: Red",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "FundaSilicona.jpg"
+                },
+                new Producto
+                {
+                    Id = 9,
+                    Nombre = "iPad Air 5",
+                    Descripcion = "Color: Gray",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "iPadAir5.jpg"
+                },
+                new Producto
+                {
+                    Id = 10,
+                    Nombre = "Cargador Original Apple",
+                    Descripcion = "Cargador 20w más cable Lightning",
+                    Precio = 420000m,
+                    Stock = 20,
+                    ImagenUrl = "CargadorConCable.jpg"
                 }
+
             );
         }
     }
